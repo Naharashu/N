444e2111b6123be8e3f436650c983f5acfb2c13e
Naharashu <130159705+Naharashu@users.noreply.github.com>
2025-05-08 22:31:57 +0300

    Add files via upload

--- c/chunk.h
+++ c/chunk.h
@@ -2,8 +2,10 @@
 #define chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -11,12 +13,19 @@ typedef struct {
     int count;
     int capacity;
     uint8_t * code;
+    ValueArray constants;
+    int* lines;
 } Chunk;
 
 void initChunk(Chunk * chunk) {
 }
+
 void writeChunk(Chunk * chunk, uint8_t byte) {}
 
+int addConstant(Chunk * chunk, Value value) {
+}
+
 void freeChunk(Chunk *chunk) {}
+
 
 #endif
